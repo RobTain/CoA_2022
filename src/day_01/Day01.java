75b74529e4b6bf1ecd54133dfe42c00d2af53bc0
Robert König <mailto.koenig.robert@gmail.com>
2022-12-01 11:39:03 +0100

    Interface added

--- src/day_01/Day01.java
+++ src/day_01/Day01.java
@@ -1,5 +1,32 @@
 package day_01;
 
-public class Day01 {
+import java.util.LinkedList;
+
+import util.CoA_Interface;
+
+public class Day01 implements CoA_Interface {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public LinkedList<String> readInput() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String result01(LinkedList<String> input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String result02(LinkedList<String> input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ src/util/CoA_Interface.java
@@ -0,0 +1,12 @@
+package util;
+
+
+import java.util.LinkedList;
+
+public interface CoA_Interface {
+	
+	public LinkedList<String> readInput();
+	public String result01(LinkedList<String> input);
+	public String result02(LinkedList<String> input);
+
+}
