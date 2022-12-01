9cd899889f5d441b863694763a71ddc585e4bb37
Robert König <mailto.koenig.robert@gmail.com>
2022-12-01 11:24:36 +0100

    Initial Push

---
+++ src/day01/Day_01.java
@@ -0,0 +1,23 @@
+package day01;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day_01 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("src/day09/input_1");
+
+		try (Scanner sc = new Scanner(file)) {
+			while (sc.hasNextLine()) {
+						
+			}
+		}
+	
+		
+		System.out.println("Solution: ");
+	}
+
+}
