587a76f7e497eeb52016b3f4aa5a5606d7978b50
Robert König <mailto.koenig.robert@gmail.com>
2022-12-01 11:44:16 +0100

    Update Day01.java

--- src/day_01/Day01.java
+++ src/day_01/Day01.java
@@ -9,21 +9,24 @@ public class Day01 implements CoA_Interface {
 	
 	public static void main(String[] args) {
 		
+		Day01 day01 = new Day01();
+		LinkedList<String> input = day01.readInput();
+		
+		System.out.println("Solution - Part 1: " + day01.result01(input));
+		System.out.println("Solution - Part 2: " + day01.result02(input));
+		
 	}
 
-	@Override
 	public LinkedList<String> readInput() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String result01(LinkedList<String> input) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String result02(LinkedList<String> input) {
 		// TODO Auto-generated method stub
 		return null;
