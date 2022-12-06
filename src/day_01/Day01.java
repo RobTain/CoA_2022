8c461c727b394c404c6f5b137d28b5e8eb370373
Robert König <mailto.koenig.robert@gmail.com>
2022-12-06 17:12:51 +0100

    Day01_added

--- src/day_01/Day01.java
+++ src/day_01/Day01.java
@@ -1,35 +1,59 @@
 package day_01;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 import util.CoA_Interface;
 
 public class Day01 implements CoA_Interface {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		Day01 day01 = new Day01();
-		LinkedList<String> input = day01.readInput();
+		LinkedList<String> input = new LinkedList<String>();
+
+		day01.readInput(input);
 		
 		System.out.println("Solution - Part 1: " + day01.result01(input));
 		System.out.println("Solution - Part 2: " + day01.result02(input));
 		
 	}
 
-	public LinkedList<String> readInput() {
-		// TODO Auto-generated method stub
-		return null;
+	public void readInput(LinkedList<String> input) throws Exception {
+		Scanner in = new Scanner(new File("src/" + this.getClass().getPackageName() + "/" + this.getClass().getSimpleName() + "_input.txt")); 
+		while(in.hasNextLine()) {
+			input.add((String) in.nextLine());
+		}
 	}
 
 	public String result01(LinkedList<String> input) {
-		// TODO Auto-generated method stub
-		return null;
+		int result = 0,temp = 0;
+		for (int i = 0; i < input.size(); i++) {
+			if (input.get(i).equals("")) {
+				if (result < temp) { result = temp; }
+				temp = 0;
+			} else { temp += Integer.parseInt(input.get(i)); }
+		}
+		return String.valueOf(result);
 	}
 
 	public String result02(LinkedList<String> input) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int temp = 0;
+		for (int i = 0; i < input.size(); i++) {
+			if (input.get(i).equals("")) { 
+				list.add(temp); 
+				temp = 0;
+			} else { temp += Integer.parseInt(input.get(i)); }
+		}
+		
+		Collections.sort(list, Collections.reverseOrder());
+		return String.valueOf(list.get(0) + list.get(1) + list.get(2));
 
+		
+		
+	}
 }
--- src/util/CoA_Interface.java
+++ src/util/CoA_Interface.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public interface CoA_Interface {
 	
-	public LinkedList<String> readInput();
+	public void readInput(LinkedList<String> input) throws Exception;
 	public String result01(LinkedList<String> input);
 	public String result02(LinkedList<String> input);
 
